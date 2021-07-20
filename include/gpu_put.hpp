a6976bf12b7924aab02d8884470cb138e2d687fe
Zhang690683220 <zhangbo_china@live.cn>
2021-07-20 14:22:51 -0400

    mv the init to hpp

--- include/gpu_put.hpp
+++ include/gpu_put.hpp
@@ -53,6 +53,81 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
                 std::vector<uint64_t>& sp, int timesteps, int var_num, int delay, int interval, 
                 std::string log_name, bool terminate)
 {
+    char* listen_addr_str = NULL;
+    if(!listen_addr.empty()) {
+        listen_addr_str = (char*) malloc(sizeof(char)*128);
+        strcpy(listen_addr_str, listen_addr.c_str());
+    }
+    int rank, nprocs;
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    Timer timer_sync;
+    Timer timer_async;
+    double put_time_async;
+
+    dspaces_client_t ndcl = dspaces_CLIENT_NULL;
+    dspaces_init(rank, &ndcl, listen_addr_str);
+
+    char var_name[128];
+    sprintf(var_name, "test_gpu_data");
+
+    double* gpu_data;
+    int dim0, dim1, dim2;
+    int ndims = 3;
+    dim0 = 64;
+    dim1 = DEFAULT_DIM;
+    dim2 = DEFAULT_DIM;
+    int size = dim0*dim1*dim2;
+
+    gpu_data = (double*) malloc(size*sizeof(double));
+
+    for(int ts=1; ts<=DEFAULT_TIMESTEP; ts++) {
+
+#pragma acc enter data create(gpu_data[0:size])
+
+    #pragma acc parallel loop collapse(3)
+    for(int i=0; i<dim0; i++) {
+        for(int j=0; j<dim1; j++) {
+            for(int k=0; k<dim2; k++) {
+                gpu_data[i*dim1*dim2+j*dim2+k] = DEFAULT_VALUE;
+            }
+        }
+    }
+
+    uint64_t lb[3] = {0}, ub[3] = {0};
+
+    ub[0] = 63;
+    ub[1] = 1023;
+    ub[2] = 1023;
+
+    dspaces_sub(ndcl, var_name, ts, sizeof(double), ndims, lb, ub, timer_cb, &timer_sync);
+    sleep(3);
+
+    #pragma acc host_data use_device(gpu_data)
+    {
+        timer_sync.start();
+        timer_async.start();
+        dspaces_put(ndcl, var_name, ts, sizeof(double), ndims, lb, ub, gpu_data);
+        put_time_async = timer_async.stop();
+    }
+
+#pragma acc exit data delete(gpu_data[0:size])
+
+    std::cout<< "DSPACES_GPU_PUT() Version = "<< ts << " TIME(ASync) = " << put_time_async << "(ms)" << std::endl;
+
+    }
+
+    dspaces_fini(ndcl);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //std::cout<< "DSPACES_GPU_PUT() TIME = " << put_time << "(ms)" << std::endl;
+
+    MPI_Finalize();
+
+    return 0;
+    /*
     int rank, nprocs;
     MPI_Comm_size(gcomm, &nprocs);
     MPI_Comm_rank(gcomm, &rank);
@@ -177,6 +252,7 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
     dspaces_fini(ndcl);
 
     return 0;
+    */
 
 }
 };
