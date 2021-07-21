3e61b1e4dc932b2435aab210e2bb0c041a3bd86e
Zhang690683220 <zhangbo_china@live.cn>
2021-07-20 20:49:37 -0400

    test

--- include/gpu_put.hpp
+++ include/gpu_put.hpp
@@ -108,7 +108,10 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
     for(int ts=1; ts<=timesteps; ts++) {
         sleep(delay);
         if((ts-1)%interval==0) {
-            #pragma acc enter data create(data_tab[0:var_num][0:grid_size])
+            for(int i=0; i<var_num; i++) {
+                #pragma acc enter data create(data_tab[i][0:grid_size])
+            }
+            
             for(int i=0; i<var_num; i++) {
                 #pragma acc parallel loop
                 for(int j=0; j<grid_size; j++) {
@@ -119,9 +122,9 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
             Timer timer_put;
             timer_put.start();
             for(int i=0; i<var_num; i++) {
-                #pragma acc host_data use_device(data_tab)
+                #pragma acc host_data use_device(data_tab[i])
                 {
-                    dspaces_put(ndcl, var_name_tab[i], ts, sizeof(float), dims, lb, ub, data_tab);
+                    dspaces_put(ndcl, var_name_tab[i], ts, sizeof(float), dims, lb, ub, data_tab[i]);
                 }
             }
             double time_put = timer_put.stop();
@@ -143,7 +146,9 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
                 total_avg += avg_put[ts-1];
                 free(avg_time_put);
             }
-            #pragma acc exit data delete(data_tab[0:var_num][0:grid_size])
+            for(int i=0; i<var_num; i++) {
+                #pragma acc exit data delete(data_tab[i][0:grid_size])
+            }
         }
     }
 
