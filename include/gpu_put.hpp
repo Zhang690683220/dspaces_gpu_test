e03e4c00a2971d3c509a2b40fe1ccacb3b9dca6f
Zhang690683220 <zhangbo_china@live.cn>
2021-07-20 23:57:31 -0400

    test

--- include/gpu_put.hpp
+++ include/gpu_put.hpp
@@ -120,7 +120,7 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
             Timer timer_put;
             timer_put.start();
             for(int i=0; i<var_num; i++) {
-                #pragma acc host_data use_device(data_tab[i:i+1])
+                #pragma acc host_data use_device(data_tab[i])
                 {
                     dspaces_put(ndcl, var_name, ts, sizeof(double), dims, lb, ub, data_tab[i]);
                 }
