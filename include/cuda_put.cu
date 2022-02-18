d0f44dd614bc632ddfce1b6e485920014ab8e48c
Zhang690683220 <zhangbo_china@live.cn>
2022-02-17 19:04:21 -0500

    re-structure cuda put file

--- include/cuda_put.cu
+++ include/cuda_put.cu
@@ -1,8 +1,25 @@
+#ifndef CUDA_PUT_HPP
+#define CUDA_PUT_HPP
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include "unistd.h"
+#include "mpi.h"
 #include <iostream>
 #include "dspaces.h"
 #include "timer.hpp"
 #include "cuda_runtime.h"
-#include "cuda_put.hpp"
+
+
+/*
+int timer_cb(dspaces_client_t client, struct dspaces_req* req, void* timer) {
+    Timer* timer_ptr = (Timer*) timer;
+    double put_time_async = timer_ptr->stop();
+    std::cout<< "DSPACES_CPU_PUT() Version = "<< req->ver << " TIME(Sync) = " << put_time_async << "(ms)" << std::endl;
+    return 0;
+}
+*/
 
 template <typename Data_t>
 struct CUDA {
@@ -20,7 +37,13 @@ __global__ void assign(double *ptr, int size, int var_idx)
 }
 };
 
-template <> struct Run<double>;
+template <typename Data_t>
+struct Run {
+    static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<int>& np,
+                    std::vector<uint64_t>& sp, int timesteps, int var_num, int delay, int interval, 
+                    std::string log_name, bool terminate);
+};
+
 
 template <>
 struct Run <double> {
@@ -163,3 +186,7 @@ static int put(MPI_Comm gcomm, std::string listen_addr, int dims, std::vector<in
 
 }
 };
+
+
+#endif // CUDA_PUT_HPP
+
